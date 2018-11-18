ff4b4bed660d5bcb4a932d44ede5a5f140ee22cb
Theodora Brisimi <theodora.brisimi@ibm.com>
2018-11-18 14:55:32 +0000

    Create Need.new view and controller

--- app/controllers/needs_controller.rb
+++ app/controllers/needs_controller.rb
@@ -4,7 +4,14 @@ class NeedsController < ApplicationController
     @needs = Need.all
   end
 
+  def new
+    @need = Need.new
+  end
 
-
+  def create
+    @need = Need.new(params[:need].permit!)
+    @need.save!
+    redirect_to '/needs'
+  end
 
 end
